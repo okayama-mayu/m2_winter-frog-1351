98a18c935f90c17becbe4f82567354393a32b362
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-08-08 11:51:02 -0500

    model: add Garden#plants_within_100_sorted

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -5,6 +5,16 @@ class Garden < ApplicationRecord
     plots
     .joins(:plants)
     .where('plants.days_to_harvest < 100')
-    .select('plants.name as name, plants.id as plant_id').group('plants.id')
+    .select('plants.name as name, plants.id as plant_id')
+    .group('plants.id')
+  end
+  
+  def plants_within_100_sorted
+    plots
+    .joins(:plants)
+    .where('plants.days_to_harvest < 100')
+    .select('plants.name as name, plants.id as plant_id, sum(plants.id) as count')
+    .group('plants.id')
+    .order('count desc')
   end
 end
