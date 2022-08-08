76f778ea9fafffa1ff1f79c9531257fa9bb4022c
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-08-08 11:36:19 -0500

    model: add Garden#plants_within_100

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,3 +1,10 @@
 class Garden < ApplicationRecord
   has_many :plots
+
+  def plants_within_100
+    plots
+    .joins(:plants)
+    .where('plants.days_to_harvest < 100')
+    .select('plants.name as name, plants.id as plant_id').group('plants.id')
+  end
 end
